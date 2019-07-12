36c76341023212f1af5d93741456446911a49676
Alex Whan <alexwhan@gmail.com>
2019-07-12 16:12:25 +1000

    animation working ish

--- R/animation.R
+++ R/animation.R
@@ -1,76 +1,88 @@
+#' @importFrom dplyr mutate
+dplyr::mutate
 
-funnel <- "12345678"
-funnel_sep <- unlist(strsplit(funnel, ""))
-#first cross
+#' @importFrom ggplot2 aes
+ggplot2::aes
 
-dat <- pedigreeburst:::data_prep(funnels_right) %>%
-  mutate(cross1 = gsub("(^.{2}).*", "\\1", funnel),
-         cross2 = gsub("(^.{4}).*", "\\1", funnel)) %>%
-  arrange(ymax) %>%
-  group_by(ymax) %>%
-  mutate(level = group_indices())
+#' Animate a funnel in a pedigreeburst
+#'
+#' @param funnels A data.frame
+#' @param funnel A string describing the funnel of interest
+#' @param show_base Logical. Should the base pedigree be shown?
+#' @param show_animation Logical. Should the animation be shown?
+#'
+#' @return
+#' @export
+#'
+#' @examples
+animate_funnel <- function(funnels, funnel, show_base = TRUE, show_animation = FALSE) {
+  if(!require(gganimate)) stop("The gganimate package is required for this function")
 
-select_funnel <- funnel
-founder_funnel <- unlist(strsplit(funnel, ""))
-cross1_funnel <- unlist(strsplit(gsub("(.{2})", "\\1 ", funnel), " "))
-cross2_funnel <- unlist(strsplit(gsub("(.{4})", "\\1 ", funnel), " "))
+  dat <- data_prep(funnels)
 
-animdat <- dplyr::bind_rows(
-  mutate(dplyr::filter(dat,
-                       cross1 == cross1_funnel[1] & level %in% 1:2),
-         state = 2),
-  mutate(dplyr::filter(dat,
-                       (cross1 == cross1_funnel[1] & level %in% 1:2) |
-                         (cross1 == cross1_funnel[2] & level %in% 1:2)),
-         state = 3),
-  mutate(dplyr::filter(dat,
-                       cross2 == cross2_funnel & level %in% 1:4),
-         state = 4),
-  # mutate(dplyr::filter(dat,
-  #                      (cross2 == cross2_funnel & level %in% 1:4) |
-  #                      (as.character(id) %in% founder_funnel[5:6] & level == 1)),
-  #        state = 5),
-  mutate(dplyr::filter(dat,
-                       (cross2 == cross2_funnel & level %in% 1:4) |
-                         (cross1 == cross1_funnel[3] & level %in% 1:2)),
-         state = 6),
-  # mutate(dplyr::filter(dat,
-  #                      (cross2 == cross2_funnel & level %in% 1:4) |
-  #                        (cross1 == cross1_funnel[3] & level %in% 1:2)|
-  #                        (as.character(id) %in% founder_funnel[7:8] & level == 1)),
-  #        state = 7),
-  mutate(dplyr::filter(dat,
-                       (cross2 == cross2_funnel & level %in% 1:4) |
-                         (cross1 == cross1_funnel[3] & level %in% 1:2)|
-                         (cross1 == cross1_funnel[4] & level %in% 1:2)),
-         state = 8),
-  mutate(dplyr::filter(dat,
-                       (cross2 == cross2_funnel & level %in% 1:4) |
-                         (cross2 == cross2_funnel[2] & level %in% 1:4)),
-         state = 9),
-  mutate(dplyr::filter(dat,
-                       funnel == select_funnel),
-         state = 10),
-  mutate(dat, state = 11)
+  dat$cross1 <- gsub("(^.{2}).*", "\\1", dat$funnel)
+
+  dat$cross2 <- gsub("(^.{4}).*", "\\1", dat$funnel)
+
+  select_funnel <- funnel
+
+  founder_funnel <- unlist(strsplit(funnel, ""))
+
+  cross1_funnel <- unlist(strsplit(gsub("(.{2})", "\\1 ", funnel), " "))
+
+  cross2_funnel <- unlist(strsplit(gsub("(.{4})", "\\1 ", funnel), " "))
+
+  animdat <- dplyr::bind_rows(
+    mutate(dplyr::filter(dat,
+                         cross1 == cross1_funnel[1] & level %in% 1:2),
+           state = 2),
+    mutate(dplyr::filter(dat,
+                         (cross1 == cross1_funnel[1] & level %in% 1:2) |
+                           (cross1 == cross1_funnel[2] & level %in% 1:2)),
+           state = 3),
+    mutate(dplyr::filter(dat,
+                         cross2 == cross2_funnel & level %in% 1:4),
+           state = 4),
+    mutate(dplyr::filter(dat,
+                         (cross2 == cross2_funnel & level %in% 1:4) |
+                           (cross1 == cross1_funnel[3] & level %in% 1:2)),
+           state = 6),
+    mutate(dplyr::filter(dat,
+                         (cross2 == cross2_funnel & level %in% 1:4) |
+                           (cross1 == cross1_funnel[3] & level %in% 1:2)|
+                           (cross1 == cross1_funnel[4] & level %in% 1:2)),
+           state = 8),
+    mutate(dplyr::filter(dat,
+                         (cross2 == cross2_funnel & level %in% 1:4) |
+                           (cross2 == cross2_funnel[2] & level %in% 1:4)),
+           state = 9),
+    mutate(dplyr::filter(dat,
+                         funnel == select_funnel),
+           state = 10),
+    mutate(dat, state = 11)
   )
 
-animp <- ggplot2::ggplot(animdat, aes(xmin = xmin, xmax = xmax,
-                         ymin = ymin, ymax = ymax + 0.01)) +
-  geom_rect(data = dat, fill = "grey", colour = "darkgrey") +
-  ggplot2::geom_rect(aes(fill = as.factor(id))) +
-  xlim(c(0, 8)) +
-  labs(title = "State: {closest_state}") +
-  ggplot2::scale_fill_brewer(name = "Founders", palette = "Dark2") +
-  ggplot2::ylim(c(-0.5, 4.4)) +
-  ggplot2::coord_polar() +
-  ggplot2::theme_minimal() +
-  ggplot2::theme(axis.text = element_blank(),
-                 axis.title = element_blank(),
-                 panel.grid = element_blank()) +
-  ggplot2::theme(legend.position = "none") +
-  transition_states(state) +
-  enter_fade() +
-  exit_shrink() +
-  ease_aes()
+  animp <- ggplot2::ggplot(animdat, aes(xmin = xmin, xmax = xmax,
+                                        ymin = ymin, ymax = ymax + 0.01)) +
+    ggplot2::geom_rect(data = dat, fill = "grey", colour = "darkgrey") +
+    ggplot2::geom_rect(aes(fill = as.factor(id))) +
+    ggplot2::xlim(c(0, 8)) +
+    ggplot2::labs(title = "State: {closest_state}") +
+    ggplot2::scale_fill_brewer(name = "Founders", palette = "Dark2") +
+    ggplot2::ylim(c(-0.5, 4.4)) +
+    ggplot2::coord_polar() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.text = ggplot2::element_blank(),
+                   axis.title = ggplot2::element_blank(),
+                   panel.grid = ggplot2::element_blank()) +
+    ggplot2::theme(legend.position = "none") +
+    gganimate::transition_states(state) +
+    gganimate::enter_fade() +
+    gganimate::exit_shrink() +
+    gganimate::ease_aes()
 
-animate(animp, rewind = FALSE)
+  if(show_animation) gganimate::animate(animp, rewind = FALSE)
+
+  return(animp)
+}
+
