247d03ba380968ca730c4acc0142c129cb1d33c2
Alex Whan <alexwhan@gmail.com>
2019-07-16 09:49:55 +1000

    no, this is the one with the layout function

---
+++ R/pedigree-graph-layout.R
@@ -0,0 +1,24 @@
+#' Pedigree graph layout
+#'
+#' @param funnels A data frame with funnels
+#'
+#' @return
+#' @export
+#'
+pedigree_graph_layout <- function(funnels) {
+
+  ped <- funnel_to_pedigree(funnels, include_zero = TRUE)
+
+  pedlg <- dplyr::group_by(dplyr::arrange(ped, gen, id), gen)
+
+  pedlm <- dplyr::mutate(pedlg, xpos = (dplyr::row_number() - 1) / (dplyr::n() - 1))
+
+  pedlnz <- pedlm[pedlm$gen != 0,]
+
+  pedlg2 <- tidyr::gather(pedlnz, rel, parentid, f, m)
+
+  pedlp <- dplyr::rename(pedlm, parentid = id, parentxpos = xpos, parentgen = gen)
+  pedlout <- dplyr::left_join(pedlg2, pedlp)
+
+  return(pedlout)
+}
